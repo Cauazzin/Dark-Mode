9bd45317c26d7b7afce6122e8f8015c5e0f91d02
Cauazzin <cauafsantos17@gmail.com>
2025-04-01 19:28:11 -0300

    Add files via upload

---
+++ DarkMode - Cauã/script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector("#btn")
+
+function darkMode() {
+    const body = document.querySelector("body")
+    body.classList.toggle("dark-mode")
+}
+
+btn.addEventListener("click", darkMode)
+
+const button = document.getElementById("btn")
+const body = document.body;
+
+button.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains(darkMode)) {
+        button.textContent = "Desativar"
+    } else {
+        button.textContent = "Ativar"
+    }
+});
